f8ebd08ecda059bec523f449a2414e65517c40b7
gouravrj <gourav.raj42001@gmail.com>
2025-12-24 20:32:39 +0530

    Update base path for new repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/gourab-s-devops-portfolio/",
+  base: "/portfolio/",
   server: {
     host: "::",
     port: 8080,
